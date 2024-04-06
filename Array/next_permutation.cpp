c368aae050e4d7d2300fb444e9b5ec016e37557b
Bruno Berndt Lima <83605421+bruno-berndt-lima@users.noreply.github.com>
2024-04-05 21:11:27 -0300

    Update next_permutation.cpp

--- Array/next_permutation.cpp
+++ Array/next_permutation.cpp
@@ -3,6 +3,8 @@ public:
     void nextPermutation(vector<int>& nums) {
         int break_point = -1;
 
+        // Find the index of the break point where starting
+        // from the end of the array, nums[i] < nums[i+1]
         for(int i =  nums.size() - 2; i >= 0; i--) {
             if(nums[i] < nums[i + 1]) {
                 break_point = i;
@@ -10,14 +12,21 @@ public:
             }
         }
 
+        // If no break point found, this means that the array are 
+        // sorted in decreasing order, then the next permutation
+        // will be the array sorted in increasing order
         if(break_point == -1) {
             reverse(nums.begin(), nums.end());
         } else {
             int larger_num_index;
             for(int i = nums.size() - 1; i >= 0; i--) {
+                // If there's a break point, starting from the end of the array,
+                // find the index of the first value larger than the break point 
                 if(nums[i] > nums[break_point]) {
                     larger_num_index = i;
+                    // Swap the break point value with the first larger value found
                     swap(nums[break_point], nums[larger_num_index]);
+                    // Finally, reverse the right side of break point 
                     reverse(nums.begin() + break_point + 1, nums.end());
                     break;
                 }
