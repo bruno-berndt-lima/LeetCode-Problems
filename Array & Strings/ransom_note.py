1a864ceffe8054d591930d59039267ecfc61a930
Bruno Berndt Lima <83605421+bruno-berndt-lima@users.noreply.github.com>
2025-03-13 14:16:16 -0300

    Create ransom_note.py

---
+++ Array & Strings/ransom_note.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        char_map = defaultdict(int)
+
+        for c in magazine:
+            char_map[c] += 1
+
+        for c in ransomNote:
+            char_map[c] -= 1
+
+            if char_map[c] < 0:
+                return False
+              
+        return True
